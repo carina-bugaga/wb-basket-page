40a29f7b14e03193214c5f9743a46f9830c76fec
Carina B <carina.bugaga@gmail.com>
2023-11-09 16:22:04 +0300

    add line in hiddenSection

--- js/modules/hiddenSection.mjs
+++ js/modules/hiddenSection.mjs
@@ -1,9 +1,10 @@
 const hiddenSection = (event) => {
   let btn = event.target
   let parent = btn.parentNode
-  let hiddenElem = parent.nextElementSibling
+  let hiddenElem = parent.parentNode.querySelector('.products-cards')
   const hiddenLabel = document.getElementById('hidden-label')
   const hiddenTitle = document.querySelector('.hidden-title')
+  const line = document.querySelector('.hidden-line')
   
   hiddenElem.classList.toggle('hidden')
   btn.classList.toggle('arrow-rotate')
@@ -11,6 +12,7 @@ const hiddenSection = (event) => {
   if (btn.dataset.top == 'true') {
     hiddenLabel.classList.toggle('hidden');
     hiddenTitle.classList.toggle('show');
+    line.classList.toggle('show')
   }
 }
 
